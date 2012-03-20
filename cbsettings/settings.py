1c5aeef71680b0b464fedea6e6b9729a9c07df07
Matthew Tretter <matthew@exanimo.com>
2012-03-20 19:09:20 -0400

    Remove Debug, Production classes

--- cbsettings/settings.py
+++ cbsettings/settings.py
@@ -6,16 +6,6 @@ class DjangoDefaults(object):
     pass
 
 
-class Debug:
-    """A mixin for a debug settings class."""
-    DEBUG = True
-
-
-class Production:
-    """A mixin for turning off debug mode."""
-    DEBUG = False
-
-
 for attr in vars(global_settings):
     if re.match(r'^[^_]', attr):
         setattr(DjangoDefaults, attr, getattr(global_settings, attr))
