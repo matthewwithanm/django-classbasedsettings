57de7574d1657eea26ede958b5eb41588435ddc3
Matthew Tretter <matthew@exanimo.com>
2012-05-10 18:31:26 -0400

    Add "testing" condition

--- cbsettings/switching/checks.py
+++ cbsettings/switching/checks.py
@@ -1,7 +1,15 @@
 import fnmatch
+import os
 import socket
+import sys
 
 
 def check_hostnames(switcher, value):
     hostname = socket.gethostname()
     return any(fnmatch.fnmatchcase(hostname, pattern) for pattern in value)
+
+
+def check_testing(switcher, value):
+    testing = [os.path.basename(sys.argv[0]), sys.argv[1]] == ['manage.py',
+            'test']
+    return testing == value
