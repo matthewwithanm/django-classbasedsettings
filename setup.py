f15835689700fea6050c1f6c909521dad641ac33
Matthew Tretter <matthew@exanimo.com>
2012-05-30 22:10:37 -0400

    PEP8

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     zip_safe=False,
     keywords=['settings', 'classbased', 'class-based'],
     include_package_data=True,
-    install_requires = [
+    install_requires=[
         'nose',
         'unittest2',
         'Django>=1.2',
